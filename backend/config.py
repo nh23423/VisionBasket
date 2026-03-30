51bf6c4526f605f81110c23aebeec778f3e8afd4
nh23423 <157596298+nh23423@users.noreply.github.com>
2026-03-30 22:39:37 +0100

    Update config for production AWS S3

--- backend/config.py
+++ backend/config.py
@@ -7,5 +7,5 @@ class Config:
     S3_SECRET = os.environ.get("AWS_SECRET_KEY", "minioadmin")
     S3_ENDPOINT = os.environ.get("S3_ENDPOINT_URL", "http://minio:9000")
     S3_PUBLIC_URL = os.environ.get("S3_PUBLIC_URL", "http://localhost:9000")
-    S3_REGION = os.environ.get("AWS_REGION", "eu-west-2")
-    BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "my-videos")
+    S3_REGION = os.environ.get("AWS_REGION", None)
+    BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", None)
